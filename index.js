1aed951d89adab8eeccc14ce8c6d52b31f8a817e
Jayamadhuri <jayamadhurirkv1@gmail.com>
2021-08-24 06:13:00 +0000

    commit message

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const element = (
-//   Write your code here.
-)
+  //   Write your code here.
+  <div className="cricket-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="images-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="RCBImage"
+        className="logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="CSKImage"
+        className="logo"
+      />
+    </div>
+  </div>
+);
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById("root"));
